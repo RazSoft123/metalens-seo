af9ef1664be0d007ad28922b9110644136ccdcc3
RazSoft <razgroupgame@gmail.com>
2026-03-14 17:17:21 +0530

    Added content script to get the title of the page and added the popup script to display the title to the title place. Using the request and response API of chrome

--- src/content.js
+++ src/content.js
@@ -1,1 +1,19 @@
 // This script will do all the things related to the current webpage
+let title = document.title;
+console.log("[From Extension] Title of webpage is: ", title);
+
+// Listen for the request from the popup to send the SEO data back
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+
+    if (request.action === "GET_SEO_DATA") {
+
+        // Collecting SEO data
+        const seoData = {
+            title: document.title
+        }
+
+        sendResponse(seoData);
+    }
+
+    return true;
+})
--- src/popup.js
+++ src/popup.js
@@ -1,1 +1,21 @@
 // The script will use to manage all the things related to popup.
+
+window.onload = async () => {
+
+    // Get current active tab 
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    chrome.tabs.sendMessage(tab.id, { action: "GET_SEO_DATA" }, (response) => {
+        const display = document.getElementById("title");
+
+        if (chrome.runtime.lastError) {
+            display.innerText = "ERROR: Plese refresh the page to see the seo Data";
+            return;
+        }
+
+        if (response) {
+            display.innerText = response.title;
+        }
+    })
+
+}
